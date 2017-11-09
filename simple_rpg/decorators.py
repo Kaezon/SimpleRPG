4d540328a0cf3ed0f74e09f416b702f8633c0051
Brett Costabile <kaezontpa@gmail.com>
2017-11-09 01:13:18 -0500

    fixed decorator

--- simple_rpg/decorators.py
+++ simple_rpg/decorators.py
@@ -1,9 +1,12 @@
 """Decorators for use with SimpleRPG"""
+from discord.ext import commands
 
 
-def must_have_character(ctx):
-    # This needs to become a database check
-    if ctx.message.author in ctx.bot.characters:
-        return True
-    ctx.send("You must create a character first!")
-    return False
+def must_have_character():
+    def predicate(ctx):
+        # This needs to become a database check
+        if ctx.message.author in ctx.bot.characters:
+            return True
+        ctx.send("You must create a character first!")
+        return False
+    return commands.check(predicate)
